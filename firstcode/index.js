0e904a35f89038e0a5b04547ced8847889bbfe0d
Reihaneh Mamdouhi <rmamdouhi57@gmail.com>
2022-08-08 11:10:36 +0430

    Add files via upload

---
+++ firstcode/index.js
@@ -0,0 +1,16 @@
+
+
+// let lapsCompleted =0
+// function incrementLap(){
+//     lapsCompleted =lapsCompleted + 1
+
+// }
+// incrementLap()
+// incrementLap()
+// incrementLap()
+
+// console.log(lapsCompleted)
+let username ="hamed"
+let message ="you have 3 notifications"
+messageToUser = username+" " + message +"!"
+console.log(messageToUser)
