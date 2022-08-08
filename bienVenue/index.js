ce0dfc1bec8b339bd9d53a03c48ff55558dd503d
Reihaneh Mamdouhi <rmamdouhi57@gmail.com>
2022-08-08 13:51:40 +0430

    Add files via upload

---
+++ bienVenue/index.js
@@ -0,0 +1,18 @@
+//grab welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl =document.getElementById("welcome-el")
+
+let esm="les apprenants"
+let greeting ="Bienvenue"
+
+
+//render welcome message
+welcomeEl.innerText = greeting +" "+ esm
+welcomeEl.innerText +="✋🏻"
+welcomeEl.style.fontSize ="50px"
+welcomeEl.style.fontFamily ="arial"
+welcomeEl.style.color ="darkblue"
+welcomeEl.style.fontWeight ="bold"
+welcomeEl.style.padding="center"
+welcomeEl.style.backgroundColor="lightpink"
+welcomeEl.style.paddingLeft="500px"
+welcomeEl.style.paddingRight="600px"
